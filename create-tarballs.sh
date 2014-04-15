8299b979f6a3f0340d7b201e8abfd493a7194a15
Clément OUDOT <clem.oudot@gmail.com>
2014-04-15 07:13:45 +0000

    Simple script to ease tarball creation

---
+++ create-tarballs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script uses the tarball of all debian packages, sign them and create distribution tarballs
+
+VERSION="2.4.39"
+
+tar zxvf paquets-openldap-debian.tar.gz
+
+dpkg-sig -p --sign builder */*.deb
+
+for i in 'lenny-64' 'wheezy-32' 'wheezy-64' 'squeeze-32' 'squeeze-64'; do
+	cd $i/
+        rm -rf *.tar.gz
+	tar cvf berkeleydb-ltb-$VERSION-$i.tar berkeleydb*deb
+        gzip berkeleydb-ltb-$VERSION-$i.tar
+	tar cvf openldap-ltb-$VERSION-$i.tar openldap*deb
+        gzip openldap-ltb-$VERSION-$i.tar
+        cd ../
+done
+
+
