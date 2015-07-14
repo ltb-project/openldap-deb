96d1a83ff8fd984afc43a0e7d0c30634474ac8aa
David COUTADEUR <david.coutadeur@gmail.com>
2015-07-14 02:58:07 +0000

    Providing OpenLDAP-LTB 2.4.41 packages (references #773 #753 #779 #780)

--- create-tarballs.sh
+++ create-tarballs.sh
@@ -2,13 +2,13 @@
 
 # This script uses the tarball of all debian packages, sign them and create distribution tarballs
 
-VERSION="2.4.39"
+VERSION="2.4.41"
 
 tar zxvf paquets-openldap-debian.tar.gz
 
 dpkg-sig -p --sign builder */*.deb
 
-for i in 'lenny-64' 'wheezy-32' 'wheezy-64' 'squeeze-32' 'squeeze-64'; do
+for i in 'lenny-64' 'wheezy-32' 'wheezy-64' 'squeeze-32' 'squeeze-64' 'jessie-64'; do
 	cd $i/
         rm -rf *.tar.gz
 	tar cvf berkeleydb-ltb-$VERSION-$i.tar berkeleydb*deb
