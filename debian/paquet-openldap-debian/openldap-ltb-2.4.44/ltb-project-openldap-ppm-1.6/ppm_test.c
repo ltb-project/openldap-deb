9b75f67c8294b61f5147135794d368eb4637a3f9
bwt <david.coutadeur@gmail.com>
2017-05-20 14:12:22 +0200

    Adds ppm 1.6 (cracklib support)

---
+++ debian/paquet-openldap-debian/openldap-ltb-2.4.44/ltb-project-openldap-ppm-1.6/ppm_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ppm.h"
+
+int main(int argc, char *argv[])
+{
+  int ret = 1;
+
+  if(argc > 0)
+  {
+    printf("Testing password : %s\n", argv[1]);
+
+    char *errmsg = NULL;
+    Entry pEntry;
+    pEntry.e_nname.bv_val="user";
+    pEntry.e_name.bv_val="user";
+  
+    ppm_test=1; // enable ppm_test for informing ppm not to use syslog
+
+    ret = check_password(argv[1], &errmsg, &pEntry);
+  
+    ber_memfree(errmsg);
+    return ret;
+
+  }
+
+  return ret;
+}
+
+
+
