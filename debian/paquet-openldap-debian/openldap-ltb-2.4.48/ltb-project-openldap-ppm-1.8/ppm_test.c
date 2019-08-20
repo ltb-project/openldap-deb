c55b12f8a403d0ace2e6ed1de9b49cf3ec405f5a
epictete <bwterminator1@free.fr>
2019-08-20 16:07:07 +0200

    include ppm release 1.8

---
+++ debian/paquet-openldap-debian/openldap-ltb-2.4.48/ltb-project-openldap-ppm-1.8/ppm_test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ppm.h"
+
+int main(int argc, char *argv[])
+{
+  int ret = 1;
+
+  if(argc > 2)
+  {
+    printf("Testing password : '%s' for user %s\n", argv[2], argv[1]);
+
+    char *errmsg = NULL;
+    Entry pEntry;
+    pEntry.e_nname.bv_val=argv[1];
+    pEntry.e_name.bv_val=argv[1];
+  
+    ppm_test=1; // enable ppm_test for informing ppm not to use syslog
+
+    ret = check_password(argv[2], &errmsg, &pEntry);
+  
+    if(ret == 0)
+    {
+      printf("Password is OK!\n");
+    }
+    else
+    {
+      printf("Password failed checks : %s\n", errmsg);
+    }
+
+    ber_memfree(errmsg);
+    return ret;
+
+  }
+
+  return ret;
+}
+
+
+
